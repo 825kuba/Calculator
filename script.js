7b2e93c9c0d22f4af4011a779103b383f9840024
825kuba <825kuba@gmail.com>
2021-11-06 12:36:11 +1300

    need to fix: see notes

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const displayPrev = document.querySelector('.display-prev');
 const displayCur = document.querySelector('.display-cur');
 
 class Calculator {
-  prevValue = 0;
-  curValue = 0;
-  operationType = '';
+  prevValue;
+  curValue;
+  operationType;
 
   constructor(calculatorBody, prevOperation, curOperation) {
     this.calculatorBody = calculatorBody;
@@ -28,9 +28,10 @@ class Calculator {
       this.changeNumberSign.bind(this)
     );
     this.calculatorBody.addEventListener('click', this.allClear.bind(this));
+    this.calculatorBody.addEventListener('click', this.doOperation.bind(this));
     this.calculatorBody.addEventListener(
       'click',
-      this.chooseOperation.bind(this)
+      this.displayResult.bind(this)
     );
   }
 
@@ -40,7 +41,7 @@ class Calculator {
     //GET NUMBER VALUE
     const value = btn.dataset.value;
     // MAX 16 CHARS CHECK
-    if (this.curOperation.textContent.length > 16) return;
+    if (this.curOperation.textContent.length >= 16) return;
     //ONLY 1 COMMA CHECK
     if (value === '.' && this.curOperation.textContent.includes('.')) return;
     //ONLY 1 ZERO IN THE BEGINNING CHECK
@@ -55,15 +56,61 @@ class Calculator {
       this.curOperation.textContent = this.curOperation.textContent.slice(1);
     //ADD NUMBER TO MEMORY
     this.curValue = +this.curOperation.textContent;
-    console.log(this.curValue);
   };
 
-  chooseOperation = function (e) {
+  doOperation = function (e) {
     const btn = e.target.closest('.operation');
     if (!btn) return;
+    // CHECK IF ANY NUMBERS EXIST
+    if (!this.curValue) return;
 
-    this.operationType = btn.dataset.value;
-    console.log(this.operationType);
+    //IF PREV VALUE IS EMPTY SET IT AND DISPLAY THE OPERATION TYPE
+    if (!this.prevValue) {
+      this.prevValue = this.curValue;
+      this.operationType = btn.dataset.value;
+    }
+
+    //ELSE IF THE PREV VALUE ALREADY EXISTS, FIRST DO THE CALCULATION AND THEN SET THE NEW OPERATION TYPE
+    else if (this.prevValue && !this.prevOperation.textContent.includes('=')) {
+      this.prevValue = this.doCalculation();
+      this.operationType = btn.dataset.value;
+      //ELSE IF THE RESULT IS BEING DISPLAYED, DISPLAY THE RESULT AND THE NEW OPERATION SYMBOL IN PREV OPERARION
+    } else {
+      this.operationType = btn.dataset.value;
+      this.prevOperation.textContent = `${this.prevValue} ${
+        this.operationType === '/' ? '÷' : this.operationType
+      }`;
+      this.curOperation.textContent = '0';
+      this.curValue = 0;
+    }
+    this.prevOperation.textContent = `${this.prevValue} ${
+      this.operationType === '/' ? '÷' : this.operationType
+    }`;
+    this.curOperation.textContent = '0';
+    this.curValue = 0;
+  };
+
+  doCalculation = function () {
+    if (this.operationType === '+') return this.prevValue + this.curValue;
+    if (this.operationType === '-') return this.prevValue - this.curValue;
+    if (this.operationType === '*') return this.prevValue * this.curValue;
+    if (this.operationType === '/') return this.prevValue / this.curValue;
+  };
+
+  displayResult = function (e) {
+    const btn = e.target.closest('.result');
+    if (!btn) return;
+    // CHECK IF NUMBERS EXIST
+    if (!this.operationType) return;
+    // DO CALCULATION AND DISPLAY IT
+    this.prevOperation.textContent = `${this.prevValue} ${
+      this.operationType === '/' ? '÷' : this.operationType
+    } ${this.curValue} =`;
+    this.prevValue = this.doCalculation();
+    this.curOperation.textContent = this.prevValue;
+    // // SET LOWER FONT SIZE IF NUMBER TOO LONG
+    // if (this.curOperation.textContent.length >= 16)
+    //   displayCur.style.fontSize = '1.5rem';
   };
 
   deleteNumber = function (e) {
@@ -71,6 +118,8 @@ class Calculator {
     if (!btn) return;
     // NUMBER EXISTS CHECK
     if (!this.curValue) return;
+    // CHECK IF RESULT IS BEING DISPLAYED
+    if (this.prevOperation.textContent.includes('=')) return;
     //REMOVE LAST CHARACTER FROM TEXT CONTENT
     this.curOperation.textContent = this.curOperation.textContent.slice(0, -1);
     // DISPLAY ZERO WHEN EVERYTHING WAS DELETED
@@ -81,7 +130,6 @@ class Calculator {
       this.curOperation.textContent = '0';
     //ADD NUMBER TO MEMORY
     this.curValue = +this.curOperation.textContent;
-    console.log(this.curValue);
   };
 
   changeNumberSign = function (e) {
@@ -91,7 +139,6 @@ class Calculator {
     this.curValue = -this.curValue;
     //DISPLAY THE NUMBER IN TEXT CONTENT
     this.curOperation.textContent = this.curValue.toString();
-    console.log(this.curValue);
   };
 
   allClear = function (e) {
@@ -108,9 +155,8 @@ class Calculator {
     this.prevValue = 0;
     this.curValue = 0;
     this.operationType = '';
-    console.log('prev', this.prevValue);
-    console.log('cur', this.curValue);
-    console.log('oper', this.curValue);
+    // // SET ORIGINAL FONT SIZE
+    // displayCur.style.fontSize = '1.8rem';
   };
 }
 
